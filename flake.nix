4f9d02e3711890dd75eaf6de042972349268e03e
hiroqn <hiroqn1008@gmail.com>
2021-12-21 13:19:10 +0900

    :sparkles: add nixpkgs-fmt (#3)

--- flake.nix
+++ flake.nix
@@ -7,13 +7,15 @@
     darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs-fmt.url = "github:nix-community/nixpkgs-fmt";
+    nixpkgs-fmt.inputs.nixpkgs.follows = "nixpkgs";
     direnv.url = "github:ruicc/direnv/support-aliases";
     direnv.flake = false;
     BlackHole.url = "github:hiroqn/nix-BlackHole";
     BlackHole.flake = false;
   };
 
-  outputs = { self, darwin, nixpkgs, home-manager, direnv, BlackHole }:
+  outputs = { self, darwin, nixpkgs, home-manager, nixpkgs-fmt, direnv, BlackHole }:
     let
       configuration = { pkgs, ... }: {
         nix.package = pkgs.nix_2_4;
@@ -24,24 +26,29 @@
           "/nix/var/nix/profiles/per-user/root/channels"
           "$HOME/.nix-defexpr/channels"
         ];
-        nixpkgs.overlays = [(final: prev: {
-          direnv = (prev.direnv.override rec {
-            buildGoModule = args: prev.buildGoModule (args // {
-              src = direnv;
-              version = "2.28.0-ruicc";
-              vendorSha256 = "sha256-P8NLY1iGh86ntmYsTVlnNh5akdaM8nzcxDn6Nfmgr84=";
+        environment.systemPackages = [
+          nixpkgs-fmt.defaultPackage."x86_64-darwin"
+        ];
+        nixpkgs.overlays = [
+          (final: prev: {
+            direnv = (prev.direnv.override rec {
+              buildGoModule = args: prev.buildGoModule (args // {
+                src = direnv;
+                version = "2.28.0-ruicc";
+                vendorSha256 = "sha256-P8NLY1iGh86ntmYsTVlnNh5akdaM8nzcxDn6Nfmgr84=";
+              });
+            }).overrideAttrs (oldAttrs: rec {
+              doCheck = false;
             });
-          }).overrideAttrs (oldAttrs: rec {
-            doCheck = false;
-          });
-          blackhole = prev.callPackage BlackHole {};
-        })];
+            blackhole = prev.callPackage BlackHole { };
+          })
+        ];
       };
     in
     {
       darwinConfigurations."veda-20210910" = darwin.lib.darwinSystem {
         system = "x86_64-darwin";
         modules = [ configuration home-manager.darwinModule ./darwin-configuration.nix ];
+      };
     };
-  };
 }
