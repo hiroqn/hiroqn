5bee37c33b801ab4ff6eb3d6e17d6871303d418f
hiroqn <909385+hiroqn@users.noreply.github.com>
2022-10-13 00:46:47 +0900

    :fire: remove ruicc edition

--- flake.nix
+++ flake.nix
@@ -13,10 +13,8 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.inputs.utils.follows = "flake-utils";
-    codex.url = "github:herp-inc/codex/add-hm-kubernetes";
+    codex.url = "github:herp-inc/codex/refactor-codex-cmd";
 
-    direnv.url = "github:ruicc/direnv/support-aliases";
-    direnv.flake = false;
     BlackHole.url = "github:hiroqn/nix-BlackHole";
     BlackHole.inputs.nixpkgs.follows = "nixpkgs";
   };
@@ -28,7 +26,6 @@
     , nixpkgs
     , home-manager
     , codex
-    , direnv
     , BlackHole
     , ...
     }:
@@ -52,19 +49,6 @@
               home-manager.users.hiroqn.imports = [ codex.hmModule."x86_64-darwin" ];
               home-manager.users.hiroqn.codex.enable = true;
               BlackHole.enable = true;
-              nixpkgs.overlays = [
-                (final: prev: {
-                  direnv = (prev.direnv.override rec {
-                    buildGoModule = args: prev.buildGoModule (args // {
-                      src = direnv;
-                      version = "2.28.0-ruicc";
-                      vendorSha256 = "sha256-P8NLY1iGh86ntmYsTVlnNh5akdaM8nzcxDn6Nfmgr84=";
-                    });
-                  }).overrideAttrs (oldAttrs: rec {
-                    doCheck = false;
-                  });
-                })
-              ];
             })
           ./darwin-configuration.nix
           BlackHole.darwinModules.default
