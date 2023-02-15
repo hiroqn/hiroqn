2d40cad19f4f511381c071bb3f7ffecd171a7c57
hiroqn <909385+hiroqn@users.noreply.github.com>
2023-02-15 18:30:24 +0900

    remove flake-compat

--- flake.nix
+++ flake.nix
@@ -4,10 +4,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
     darwin.url = "github:LnL7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
