c72a2358dcdb78e136e27eb488497a2c23b15124
hiroqn <909385+hiroqn@users.noreply.github.com>
2024-04-08 01:07:08 +0900

    :sparkles:
    up
    
    Update default.nix
    
    Automated changes

--- darwin.nix
+++ darwin.nix
@@ -3,9 +3,4 @@
   services.nix-daemon.enable = true;
   nix.configureBuildUsers = true;
   nix.package = pkgs.nixUnstable;
-  nix.extraOptions = ''
-    netrc-file = /etc/nix/netrc
-    experimental-features = nix-command flakes
-  '';
-  nixpkgs.config.allowUnfree = true;
 }
